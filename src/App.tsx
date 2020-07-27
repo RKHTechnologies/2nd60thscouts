07f0d0a8ebed9613ca653205dfa9d074b82902a1
Rhys Howard <Rhys.Howard@theaccessgroup.com>
2020-07-27 17:17:35 +0100

    SC test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background: #444;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          2nd 60th Scouts
-        </p>
-      </header>
-    </div>
+    <AppContainer>2nd 60th Scouts</AppContainer>
   );
 }
 
