3d292a23de52a926d92599a42acee24f1cd84cde
RKH <rhysh6@gmail.com>
2020-08-02 14:54:21 +0100

    Updates

---
+++ precache-manifest.be47227f8d60e20a787c08d9b8d28e75.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2fb769675fc62491d9f34e151eb21c6",
+    "url": "/2nd60thscouts/index.html"
+  },
+  {
+    "revision": "8d516255c39185e5c9bd",
+    "url": "/2nd60thscouts/static/css/main.273edd5c.chunk.css"
+  },
+  {
+    "revision": "1410f8aaab05c3dfc87c",
+    "url": "/2nd60thscouts/static/js/2.b9b59fda.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/2nd60thscouts/static/js/2.b9b59fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d516255c39185e5c9bd",
+    "url": "/2nd60thscouts/static/js/main.314190ec.chunk.js"
+  },
+  {
+    "revision": "7e9dcc1d558cedc56500",
+    "url": "/2nd60thscouts/static/js/runtime-main.068863df.js"
+  },
+  {
+    "revision": "18bcf9c30c85f0a046eb43b18aea046e",
+    "url": "/2nd60thscouts/static/media/beavers-and-leader.18bcf9c3.jpg"
+  },
+  {
+    "revision": "a2153e192117e3c11605c7948ee8b9cf",
+    "url": "/2nd60thscouts/static/media/beavers-singing.a2153e19.jpg"
+  },
+  {
+    "revision": "8ef3972105b88eab7683b23a289c0844",
+    "url": "/2nd60thscouts/static/media/beavers.8ef39721.png"
+  },
+  {
+    "revision": "7b3594aacf63bf4b6ec5953d70348c24",
+    "url": "/2nd60thscouts/static/media/canoes.7b3594aa.jpg"
+  },
+  {
+    "revision": "0836c1746ee1b8c8cfa8206ff8e4f8bb",
+    "url": "/2nd60thscouts/static/media/cub-high-ropes.0836c174.jpg"
+  },
+  {
+    "revision": "98fd05395fdd4c31d89c2b414883b073",
+    "url": "/2nd60thscouts/static/media/cubs-group.98fd0539.jpg"
+  },
+  {
+    "revision": "a387f019489acf533f5f4e9da5b50071",
+    "url": "/2nd60thscouts/static/media/cubs-marshmallow.a387f019.jpg"
+  },
+  {
+    "revision": "7e385dd5d21f5fe4854d9360417b32fe",
+    "url": "/2nd60thscouts/static/media/explorer-expedition.7e385dd5.jpg"
+  },
+  {
+    "revision": "cf2a4ecdb7edcf2482dcb1cfd2ecc8f6",
+    "url": "/2nd60thscouts/static/media/explorer-teamwork-3.cf2a4ecd.jpg"
+  },
+  {
+    "revision": "79a336f083ed0738480f8a26d92bd763",
+    "url": "/2nd60thscouts/static/media/explorers.79a336f0.png"
+  },
+  {
+    "revision": "21ca0aab048ebc345b2c88212dc6b4b2",
+    "url": "/2nd60thscouts/static/media/group-of-scouts-in-helmets.21ca0aab.jpg"
+  },
+  {
+    "revision": "3d0fc3554c13927fb85b4efb40591f7d",
+    "url": "/2nd60thscouts/static/media/kayaking.3d0fc355.jpg"
+  },
+  {
+    "revision": "20b0173379a4fc18755b63643942478b",
+    "url": "/2nd60thscouts/static/media/logo-v.20b01733.svg"
+  },
+  {
+    "revision": "ba80e1f778c2ad6f555b4d94d97de0ee",
+    "url": "/2nd60thscouts/static/media/logo.ba80e1f7.svg"
+  },
+  {
+    "revision": "cf4ba15c251e81880fae5a848c456624",
+    "url": "/2nd60thscouts/static/media/scouts-in-a-row.cf4ba15c.jpg"
+  },
+  {
+    "revision": "83ac1664a3f8c62b43fc0f7518551acd",
+    "url": "/2nd60thscouts/static/media/scouts-life-jackets.83ac1664.jpg"
+  },
+  {
+    "revision": "e64e0482ab47641ca114d3ac0e1aeff8",
+    "url": "/2nd60thscouts/static/media/scouts.e64e0482.png"
+  },
+  {
+    "revision": "7517573ce1236e2f1f8988ad5ba3f587",
+    "url": "/2nd60thscouts/static/media/skillsForLife.7517573c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/2nd60thscouts/precache-manifest.537179ba7e3f28a86fd8a64f79ffd2e5.js"
+  "/2nd60thscouts/precache-manifest.be47227f8d60e20a787c08d9b8d28e75.js"
 );
 
 self.addEventListener('message', (event) => {
