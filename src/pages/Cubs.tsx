ddf8e2205a8fabce8c2b3541e21b129cf2718e96
RKH <rhysh6@gmail.com>
2020-08-16 17:59:05 +0100

    cubs page started

--- src/pages/Cubs.tsx
+++ src/pages/Cubs.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 import Hero from '../Shared/Hero';
-import { PageBodyContainer } from '../Shared/SharedStyles';
+import { PageBodyContainer, colours } from '../Shared/SharedStyles';
 import { Section } from './Home';
 import cubs from '../img/cubs.png';
 import { PageLogoGap, PageHeading, Grid, GridItem, Icon, Title, Description } from './Beavers';
@@ -22,8 +22,8 @@ interface ILogoProps {
 export const PageLogo = styled.img`
     position: absolute;
     top: ${(p:ILogoProps) => `${p.top}%`};
-    left: calc(50% - 400px);
-    width: 800px;
+    left: calc(50% - 300px);
+    width: 600px;
 
     @media(max-width: 850px) {
       top: ${(p:ILogoProps) => `${p.top + 4}%`};
@@ -44,6 +44,13 @@ export const PageLogo = styled.img`
     }    
 `;
 
+const CubsIcon = styled(Icon)`
+  color: ${colours.Green};
+`;
+const CubsTitle = styled(Title)`
+  color: ${colours.Green};
+`;
+
 const Cubs: FC = () => {
     return (
       <PageBodyContainer>
@@ -53,41 +60,41 @@ const Cubs: FC = () => {
         
         <Section id="about">
           <div className="wrapper">
-            <PageHeading>Beaver Scouts are our youngest members. They usually meet weekly to take part in a wide range of activities including games, crafts, singing, visits and good turns, along with plenty of outdoor activities. They will also have the opportunity to take part in the fun and excitement of camps and sleepovers. It may be the first time they spend a night away from home so it’s a real adventure for them.</PageHeading>
+            <PageHeading>A Cub Scout Pack can have up to 36 Cub Scouts and is split into smaller groups called Sixes. Cubs take part in a wide range of activities designed to be interesting and challenging. A Cub Scout meeting consists of games and activities with plenty of time spent outdoors. Camps and holidays are some of the most memorable events of the year for Cubs.</PageHeading>
             <Grid>
               <GridItem>
-                <Icon icon={faSmile} size="2x" />
-                <Title>Friendly Group</Title>
+                <CubsIcon icon={faSmile} size="2x" />
+                <CubsTitle>Friendly Group</CubsTitle>
                 <Description>Above all we are happy to help make your move as seamless as possible</Description>
               </GridItem>
               
               <GridItem>
-                <Icon icon={faChild} size="2x" />
-                <Title>6 - 8 Year olds</Title>
+                <CubsIcon icon={faChild} size="2x" />
+                <CubsTitle>6 - 8 Year olds</CubsTitle>
                 <Description>We'll sort everything out for you, taking away the stress and hassle of your move</Description>
               </GridItem>
 
               <GridItem>
-                <Icon icon={faPaintBrush} size="2x" />
-                <Title>Arts and Crafts</Title>
+                <CubsIcon icon={faPaintBrush} size="2x" />
+                <CubsTitle>Arts and Crafts</CubsTitle>
                 <Description>Our experienced colleagues can provide help with fitting and adjusting packaged items</Description>
               </GridItem>
               
               <GridItem>
-                <Icon icon={faFutbol} size="2x" />
-                <Title>Games</Title>
+                <CubsIcon icon={faFutbol} size="2x" />
+                <CubsTitle>Games</CubsTitle>
                 <Description>We love to have a good time, and learn to work as a team</Description>
               </GridItem>
 
               <GridItem>
-                <Icon icon={faCampground} size="2x" />
-                <Title>Camps and Sleepovers</Title>
+                <CubsIcon icon={faCampground} size="2x" />
+                <CubsTitle>Camps and Sleepovers</CubsTitle>
                 <Description>This cost effective solution allows you to simply pay for the time used</Description>
               </GridItem>
 
               <GridItem>
-                <Icon icon={faCompass} size="2x" />
-                <Title>Life Skills</Title>
+                <CubsIcon icon={faCompass} size="2x" />
+                <CubsTitle>Life Skills</CubsTitle>
                 <Description>As a customer driven organisation, your happiness is our priority</Description>
               </GridItem>
             </Grid>
