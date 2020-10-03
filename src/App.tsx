dcee904362030b4e890404dcd24b4c8a7ae874fc
RKH Technologies <rhysh6@gmail.com>
2020-10-03 10:41:23 +0100

    Changing config again

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import GlobalStyle from './Shared/GlobalStyle';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Error404 from './Shared/Error404';
 import HeaderBar from './Shared/HeaderBar';
 import Home from './pages/Home';
